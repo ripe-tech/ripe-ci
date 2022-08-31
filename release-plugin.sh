6086e88c03789855c45a743425233b7806cdc053
João Dias Conde Azevedo <up201503256@fe.up.pt>
2022-08-31 17:42:40 +0100

    Update release-plugin.sh

--- release-plugin.sh
+++ release-plugin.sh
@@ -13,7 +13,7 @@ echo "[INFO] Automating release of $name@$version"
 
 # builds the release bundle
 echo "[INFO] Building release bundle"
-npm install 
+npm install
 npm run build-cdn
 
 # installs the GitHub CLI tool
@@ -21,12 +21,12 @@ gh_version="2.14.7"
 echo "[INFO] Installing GitHubCLI@$gh_version"
 curl -LGO "https://github.com/cli/cli/releases/download/v${gh_version}/gh_${gh_version}_linux_amd64.tar.gz" -o gh_${gh_version}_linux_amd64.tar.gz
 tar xf gh_${gh_version}_linux_amd64.tar.gz
-gh=./../gh_${gh_version}_linux_amd64/bin/gh
 
 # logs in using the GitHub CLI tool and sets git credentials
 echo "[INFO] Logging in GitHub CLI tool and setting Git credentials"
-echo $GITHUB_TOKEN | $gh auth login --with-token
-git config --global user.name $GITHUB_ACTOR
+echo $GITHUB_TOKEN | gh_${gh_version}_linux_amd64/bin/gh auth login --with-token
+git config --global user.email "ripe-bot@platforme.com"
+git config --global user.name "ripe-bot"
 
 # clones RIPE Static
 echo "[INFO] Cloning RIPE Static"
@@ -50,11 +50,11 @@ echo "[INFO] Copying release contents to RIPE Static"
 mkdir -p ripe/ripe_commons/plugins/$name/$version
 cp -r ../dist/assets ripe/ripe_commons/plugins/$name/$version
 cp ../dist/bundle.js ripe/ripe_commons/plugins/$name/$version
-cp ../plugin.json ripe/ripe_commons/plugins/$name/$version
+cp plugin.json ripe/ripe_commons/plugins/$name/$version
 rm -rf ripe/ripe_commons/plugins/$name/latest
 cp -r ../dist/assets ripe/ripe_commons/plugins/$name/latest
 cp ../dist/bundle.js ripe/ripe_commons/plugins/$name/latest
-cp ../plugin.json ripe/ripe_commons/plugins/$name/latest
+cp plugin.json ripe/ripe_commons/plugins/$name/latest
 
 # pushes the release files to the created branch
 echo "[INFO] Pushing changes to '$branch'"
@@ -65,6 +65,6 @@ git push -u origin $branch
 # creates the release pull request
 title="version: $name@$version"
 body="version: $name@$version"
-assignee=$GITHUB_ACTOR
+assignee="ripe-bot"
 echo "[INFO] Creating pull request '$title' for branch '$branch' authored by $assignee"
-$gh pr create --assignee $assignee --title "$title" --body "$body" --label "no-changelog" --label "release 🎉"
+../gh_${gh_version}_linux_amd64/bin/gh pr create --assignee $assignee --title "$title" --body "$body" --label "no-changelog" --label "release 🎉"
